95ce1a03d99ede1b179328d80bcaa9f7b3a59607
Md.Faysal Hasan <faysalhasanmd393@mail.com>
2025-09-12 00:40:55 +0600

    get all api and showing UI

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const listContainer = document.getElementById("list-container");
+const news_Container = document.getElementById("newsContainer");
 
 const loadData = () => {
   fetch("https://news-api-fs.vercel.app/api/categories")
@@ -25,15 +26,41 @@ const cate = (cat) => {
   });
   listContainer.addEventListener("click", (e) => {
     const totalLi = document.querySelectorAll("li");
-
     totalLi.forEach((li) => {
       li.classList.remove("border-b-4");
     });
 
     if (e.target.localName === "li") {
+      // console.log(e.target.id);
       e.target.classList.add("border-b-4");
+      dataId(e.target.id);
     }
   });
 };
 
+const dataId = (catId) => {
+  console.log(catId);
+  fetch(`https://news-api-fs.vercel.app/api/categories/${catId}`)
+    .then((res) => res.json())
+    .then((data) => {
+      showCategory(data.articles);
+    });
+};
+
+const showCategory = (news) => {
+  news_Container.innerHTML = "";
+  news.forEach((newsId) => {
+    news_Container.innerHTML += `
+    <div class="border border-gray-200 rounded-[9px]">
+       <div>
+       <img src="${newsId.image.srcset[5].url}"/>
+       </div>
+    <h1 class="font-semibold text-[18px] mt-1 p-1">${newsId.title}</h1>
+    <h1 class="font-sm p-1">${newsId.time}</h1>
+    </div>
+    
+    `;
+  });
+};
 loadData();
+dataId("main");
